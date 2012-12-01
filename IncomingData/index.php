ecc8c804640296bb6daf5c0b9200212ecf814e1c
Paul Suh <plsuh@Paul-Suh-Core-i5.local>
2012-12-01 17:28:19 -0500

    updated to talk to mongodb

--- IncomingData/index.php
+++ IncomingData/index.php
@@ -2,24 +2,39 @@
 
 	require 'vendor/autoload.php';
 
-//	require "Slim/Slim.php";
-//	require "Slim/Log.php";
-//	require "Slim/Environment.php";
-//	require "Slim/Http/Request.php";
-//	require "Slim/Http/Response.php";
-//	require "Slim/Http/Headers.php";
-//	require "Slim/Router.php";
-//require 'Slim/Slim.php';
-
-//\Slim\Slim::registerAutoloader();
-
 	$app = new \Slim\Slim();
 
 	// add new Route 
-$app->get('/hello/:name', function ($name) {
-    echo "Hello, $name";
-});
-	
+	$app->get('/hello/:name', function ($name) {
+		echo "Hello, $name";
+	});
+
+	// accept new info
+	$app->put( '/:geoinfo', function( $geoinfo ) {
+		
+		//echo "$geoinfo";
+		
+		$doc_array = json_decode($geoinfo, true);
+		echo $doc_array;
+				
+		$m = new Mongo( "mongodb://localhost:27017" );
+		$db = $m->selectDB( "sanhack" );
+		$collection = $db->accradata;
+		$collection->insert( $doc_array );
+		
+/*
+		$collection->insert( 
+			array( 
+				"lat" => "30",
+				"long" => "31",
+				"timestamp" => "2012-12-01 12:34:56",
+				"truckid" => "17"
+			)
+		);
+*/
+		
+	} );
+		
 	// run the Slim app
 	$app->run();
 ?>
